273570ddd7f3715f48995bed2589a2159ff108ba
Omer Dolev <omerdolev90@gmail.com>
2023-07-11 01:00:41 +0300

    cleanup

--- travelog-app/src/main.tsx
+++ travelog-app/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App.tsx'
+
+import './index.css'
 
 const queryClient = new QueryClient();
 
